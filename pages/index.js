f70e4aa74fc440334696d699bb8e3be291a2c5c4
Justin Morrison <morrisonak@yahoo.com>
2021-12-05 22:02:46 -0600

    remove cards

--- pages/index.js
+++ pages/index.js
@@ -33,28 +33,7 @@ export default function Home({ isConnected }) {
             <p>Submit a link here to be added to to the links under consideration.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          
         </div>
       </main>
 
